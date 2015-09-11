ce6d816dc8470bf06cf149410fdfb01dbbc9f733
zack-vii <zack.vii@live.com>
2015-09-11 14:04:59 +0200

    debug tools: testarchive, generateshot
    test of Time...

---
+++ python/generatetestshot.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 12:38:35 2015
+
+@author: Cloud
+"""
+import archive
+archive.support.getTimestamp()
---
+++ python/testarchive.py
@@ -0,0 +1,10 @@
+"""
+Created on Fri Sep 11 13:08:04 2015
+testarchive
+@author: Cloud
+"""
+import archive
+print('<local time>')
+print(archive.Time().local())
+print('1234567890123456789')
+print(archive.Time('2009-02-13T23:31:30.123456789Z'))
