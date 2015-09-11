aaf242a1ecb9500534fe9ad4f18348a6fda7a75f
zack-vii <zack.vii@live.com>
2015-09-11 14:02:36 +0200

    bytes as part of basestring

--- python/version.py
+++ python/version.py
@@ -29,7 +29,9 @@ else:
     long = int
 if has_basestring:
     basestring = basestring
-else: 
+elif has_bytes: 
+    basestring = (str,bytes)
+else:
     basestring = str
 if has_unicode:
     unicode = unicode
