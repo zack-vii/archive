80728bdc2cb0d34ef9198581f704d3634b8bff22
zack-vii <zack.vii@live.com>
2015-09-11 21:06:31 +0200

    fixed archive_signal _time option

--- tdi/archive_signal.py
+++ tdi/archive_signal.py
@@ -1,27 +1,24 @@
-def archive_signal(node,time=None):
-    import archive
+import archive,MDSplus
+def archive_signal(node,time=MDSplus.EmptyData()):
+    print('archive_signal')
     try:
+        time = time.data;
         if time is None:
-            time = archive.TimeInterval(node.getNode('\TIME').data())
-        else:
             time = archive.TimeInterval(time)
+        except:
+            time = archive.TimeInterval(node.getNode('\TIME').data())
 #        try:
 #            t0 = codac.Time(node.getNode('\TIME.T1:IDEAL')).ns()
 #        except:
         t0 = time.getFrom()
-#        try:
-#            idx = node.getNode('$IDX').data()
-#            url  = node.getParent().getNode('$URL').data()
-#            signal = codac.read_signal(url,time,t0,0,0,[idx])
-#        except:
-        url  = node.getNode('$URL').data()
-        signal = codac.read_signal(url,time,t0,0,0,[])
+        url  = node.getNode(':$URL').data()
+        signal = archive.read_signal(url,time,t0,0,0,[])
         try:
-            help = node.getNode('.DESCRIPTION').data()
+            help = node.getNode(':DESCRIPTION').data()
         except:
             help = None
         if help is None: 
-            help = node.getNode('$NAME').data()
+            help = node.getNode(':$NAME').data()
         signal.setHelp(str(help))
         return(signal)
     except:
