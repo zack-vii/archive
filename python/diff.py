e7de4f5a26bfafb86f0aab2fc4246f1cf8d00e39
zack-vii <zack.vii@live.com>
2015-09-14 19:09:30 +0200

    implementation of a diff for trees
    tree to dict -> deepdiff dict

--- python/diff.py
+++ python/diff.py
@@ -1,21 +1,23 @@
 from __future__ import print_function
+from archive import deepdiff, version
+from pprint import pprint
 
 
 def diff(treename1, shot1, treename2, shot2):
-    from archive import deepdiff as dd
-    from pprint import pprint
     import MDSplus
     tree1 = MDSplus.Tree(treename1, shot1)
     tree2 = MDSplus.Tree(treename2, shot2)
-    res = dd.DeepDiff(treeToDict(tree1), treeToDict(tree2))
-    pprint(res)
-    return res
+    return treeToDict(tree1),treeToDict(tree2)
 
 
 def treeToDict(tree):
+    global i
+    i = 0
     def nodeToDict(node):
+        global i
+        i += 1
         dic = {}
-        dic["usage"] = node.usage
+        dic["usage"] = version.tostr(node.usage)
         try:
             dic["record"] = node.record
         except:
@@ -27,12 +29,15 @@ def treeToDict(tree):
         dic["do_not_compress"] = node.do_not_compress
         dic["include_in_pulse"] = node.include_in_pulse
         dic["setup_information"] = node.setup_information
-        dic["tags"] = node.tags.tolist()
+        dic["tags"] = list(map(version.tostr,node.tags.tolist()))
+        if (i % 1000) == 0: print(node.getFullPath())
         descdic = {}
-        for desc in list(node.getDescendants())[0:5]:
+        for desc in list(node.getDescendants()):
             descdic[str(desc.getNodeName())] = nodeToDict(desc)
         dic['descendants'] = descdic
         return dic
     return nodeToDict(tree.getNode('\Top'))
 
-d = diff('test', -1, 'test', 1)
+d = diff('w7x', 1, 'w7x', 2)
+res = deepdiff.DeepDiff(*d)
+pprint(res)
