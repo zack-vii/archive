c04635bb40839d11f7f6a4cb704839169d7dfff7
zack-vii <zack.vii@live.com>
2015-10-22 19:36:50 +0200

    documentation of mdsupload
    renamed and commented a lot

--- python/mdsupload.py
+++ python/mdsupload.py
@@ -18,7 +18,8 @@ _subtrees  = ['QMC','QMR','QRN','QSD','QSQ','QSR','QSW','QSX']
 
 
 def setupTiming():
-    """should be executed only once before frist experiment"""
+    """sets up the parlog of the shots datastream in the web archive
+    should be executed only once before frist experiment"""
     def chanDesc(n):
         if n==0:
             return {'name':'shot','physicalQuantity':{'type':'none'}}
@@ -30,8 +31,9 @@ def setupTiming():
     return result
 
 
-def uploadModel(shot, subtrees=_subtrees, treename='W7X'):
-    """should be executed right after T0"""
+def uploadModel(shot, subtrees=_subtrees, treename='W7X', T0=None):
+    """uploads full model tree of a given shot into the web archive
+    should be executed right after T0"""
     def getModel():
         nodenames = ['ADMIN','TIMING']+subtrees
         w7x = _mds.Tree(treename,-1)
@@ -40,15 +42,19 @@ def uploadModel(shot, subtrees=_subtrees, treename='W7X'):
             print('reading %s' % key)
             model[key]=_diff.treeToDict(w7x.getNode(key))
 
-    time = _sup.getTiming(shot, 0)
+    if T0 is None:
+        T0 = _sup.getTiming(shot, 0)
+    else:
+        T0 = _base.Time(T0)
     cfglog = getModel()
-    result = _if.write_logurl(_MDS_shots.url_cfglog(), cfglog, time)
+    result = _if.write_logurl(_MDS_shots.url_cfglog(), cfglog, T0)
     print(result.msg)
     return result
 
 
 def uploadTiming(shot):
-    """should be executed soon after T6"""
+    """uploads the timing of a given shot into the web archive
+    should be executed soon after T6"""
     data = _np.uint64(_sup.getTiming(shot))
     dim = data[0]
     data[0] = int(shot)
@@ -57,142 +63,171 @@ def uploadTiming(shot):
     return result
 
 
-def uploadShot(shot, subtrees=_subtrees, treename='W7X', time=None):
-    '''
-    uploadData(['KKS'], shot=0, treename='W7X')
-    uploads all sections of a given tree
-    e.g.: D=uploadData(['QMC', 'QMR', 'QRN', 'QSW', 'QSX'], 2, 'W7X')
-    '''
-    SD = []
+def uploadShot(shot, subtrees=_subtrees, treename='W7X', T0=None, T1=None):
+    """uploads the data of all sections of a given shot into the web archive
+    should be executed after all data is written to the shot file"""
+    sectionDicts = []
     w7x = _mds.Tree(treename, shot)
-    if time is None:
-        time = _base.TimeInterval(w7x.getNode('\TIME'))
+    if T0 is None:
+        T0 = _sup.getTiming(shot, 0)
     else:
-        time = _base.TimeInterval(time)
+        T0 = _base.Time(T0)
+    if T1 is None:
+        T1 = _sup.getTiming(shot, 0)
+    else:
+        T1 = _base.Time(T1)
     path = _base.Path('raw/W7X')
     for subtree in subtrees:
         kks = w7x.getNode(subtree)
         kkscfg = _getCfgLog(kks)
         data = kks.getNode('DATA')
-        secdict = {}
         for sec in data.getDescendants():
             print(sec)
-            path.streamgroup = subtree.upper()+'_'+sec.getNodeName().lower()+'1'
+            path.streamgroup = subtree.upper()+'_'+sec.getNodeName().lower()
             try:
-                cfg = _treeToDict(sec,kkscfg.copy(),'')
-                pcl = _if.write_logurl(path.url_cfglog(), cfg, time.fromT)
+                cfglog = _treeToDict(sec,kkscfg.copy(),'')
+                log_cfglog = _if.write_logurl(path.url_cfglog(), cfglog, T0)
             except:
-                pcl = _sup.error(1)
-            ch, secdict = _sectionDict(sec, secdict, kks, time, path)
-            SD.append({"ch": ch, "pcl": pcl})
-    return(SD)
+                log_cfglog = _sup.error(1)
+            sectionDict = _sectionDict(sec, kks, T0, T1, path)
+            sectionDicts.append({'sectionDict': sectionDict, "cfglog": log_cfglog})
+    return(sectionDicts)
 
 
-def _getCfgLog(node):
-    parms = {}
-    for m in node.getMembers():
-        parms  = _treeToDict(m,parms)
-    for c in node.getChildren():
+def _getCfgLog(kks):
+    """generates the base cfglog of a kks subtree
+    called by uploadShot"""
+    cfglog = {}
+    for m in kks.getMembers():
+        cfglog  = _treeToDict(m,cfglog)
+    for c in kks.getChildren():
         if not c.getNodeName() in ['HARDWARE','DATA']:
-            parms = _treeToDict(c,parms)
-    return(parms)
+            cfglog = _treeToDict(c,cfglog)
+    return(cfglog)
 
 
-def _sectionDict(node, secs, kks, time, path):
-    ''' for interenal use
-    browse sections of .DATA
-    '''
-    f = _re.compile('(?<=\.HARDWARE[:\.])([^\.:]+)')
-    chans = {}
-    devs = {}
-    CH = []
+def _sectionDict(section, kks, T0, T1, path):
+    """generates the parlog for a section under .DATA and upload the data
+    called by uploadShot"""
+    signalDict = {}
+    deviceDict = {}
+    sectionDict = []
     try:
-        for d in node.getDescendants():
-            chans = _channelDescs(d, chans)
-        secs[str(node.getNodeName())] = chans
+        for signal in section.getDescendants():
+            signalDict = _signalDict(signal, signalDict)
         HW = kks.getNode('HARDWARE')
         if HW.getNumDescendants()>0:
+            channelLists = _getChannelLists(kks)
             for dev in HW.getDescendants():
-                devs[dev.Nid] = []
-            for ch in chans.keys():
-                devName = f.search(str(kks.tree.getNode(ch).FullPath)).group(0)
-                dev = HW.getNode(devName)
-                devs[dev.nid].append(ch)
-            for dev in HW.getDescendants():
-                if len(devs[dev.Nid]):
+                if len(channelLists[dev.Nid]):
                     path.stream = dev.getNodeName().lower()
-                    par, sig = _iterateDevices(dev, devs[dev.Nid], chans)
+                    deviceDict, signalList = _deviceDict(dev, channelLists[dev.Nid], signalDict)
                     try:
-                        rpl = _if.write_logurl(path.url_parlog(), par, time.fromT)
+                        log_parlog = _if.write_logurl(path.url_parlog(), deviceDict, T0)
                     except:
-                        rpl = _sup.error()
-                    rd = _write_signals(path, sig, time.t0T)
-                    CH.append({"path": path.path(),
-                               "par": par, "sig": sig, "rpl": rpl, "rd": rd})
+                        log_parlog = _sup.error()
+                    log_signal = _write_signals(path, signalList, T1)
+                    sectionDict.append({"path": path.path(),
+                               "deviceDict": deviceDict,
+                               "signalList": signalList,
+                               "log": {'parlog':log_parlog, 'signal':log_signal}})
     except:
         _sup.error()
-    return(CH, secs)
+    return sectionDict
 
 
-def _channelDescs(ch, dic):
-    if ch.usage == "SIGNAL":
+def _signalDict(signal, signalDict):
+    """collects the properties of a signal node and assiciates it with a channel nid
+    called by _sectionDict"""
+    if signal.usage == "SIGNAL":
         desc = {}
-        if ch.getNumDescendants()>0:
-            _treeToDict(ch, desc)
-        desc["name"] = ch.getNodeName().lower()
-        nid = ch.getData().Nid
-        dic[nid] = desc
-    return dic
+        if signal.getNumDescendants()>0:
+            _treeToDict(signal, desc)
+        desc["name"] = signal.getNodeName().lower()
+        nid = signal.getData().Nid
+        signalDict[nid] = desc
+    return signalDict
 
 
-def _iterateDevices(node, chlist, chans):
-    parms = {}
-    signals = []
-    descs = []
-    for des in node.getDescendants():
-        sig = None
-        if des.usage == "SIGNAL":
-            sig = des
-        elif des.usage == "STRUCTURE":
-            for m in des.getMembers():
-                if m.usage == 'SIGNAL':
-                    sig = m
-        if sig is None:
-            _treeToDict(des, parms)
-        elif sig.Nid in chlist:
-            descs.append(_signalDict(des, sig, chans))
-            signals.append(sig)
-    parms["chanDescs"] = descs
-    return(parms, signals)
+def _getChannelLists(kks, channels):
+    """collects the channel lists of all devices
+    called by _sectionDict"""
+    f = _re.compile('(?<=\.HARDWARE[:\.])([^\.:]+)')
+    HW = kks.getNode('HARDWARE')
+    channelLists = dict([device.Nid, []] for device in HW.getDescendants())
+    for channel in channels.keys():
+        deviceName = f.search(str(kks.tree.getNode(channel).FullPath)).group(0)
+        device = HW.getNode(deviceName)
+        channelLists[device.nid].append(channel)
+    return channelLists
 
 
-def _signalDict(node, sig=None, dic={}):
-    if sig is None:
-        sig = node
-    desc = {}
-    desc['name'] = node.getNodeName().lower()
-    desc["active"] = int(node.isOn())
-    desc["physicalQuantity"] = {'type': 'unknown'}
-    nid = sig.Nid
-    for des in node.getDescendants():
-        if not des.Nid == nid:
-            desc = _treeToDict(des, desc)
-    if nid in dic.keys():
-        for k, v in dic[nid].items():
-            desc[k] = v
-    if 'units' in desc.keys():
-        desc["physicalQuantity"]['type'] = _base.Units(desc["units"], 1)
-        del(desc["units"])
+def _deviceDict(device, channelList, signalDict):
+    """collects the data of channels and their parenting devices
+    called by _sectionDict"""
+    def _searchSignal(descendant):
+        """checks DEVICE:SIGNAL and DEVICE.STRUCTURE:SIGNAL"""
+        if descendant.usage == "SIGNAL":
+            return descendant
+        elif descendant.usage == "STRUCTURE":
+            for member in descendant.getMembers():
+                if member.usage == 'SIGNAL':
+                    return member
+        return None
+
+    deviceDict = {}
+    signalList = []
+    chanDescs = []
+    for descendant in device.getDescendants():
+        signal = _searchSignal(descendant)
+        if signal is None:  # add to parlog
+            _treeToDict(descendant, deviceDict)
+        elif signal.Nid in channelList:  # add to signal list
+            chanDescs.append(_chanDesc(descendant, signal, signalDict))
+            signalList.append(signal)
+    deviceDict["chanDescs"] = chanDescs
+    return(deviceDict, signalList)
+
+
+def _chanDesc(signalroot, signal, signalDict={}):
+    """generates the channel descriptor for a given channel"""
+    def mergeSignalDict():
+        if nid in signalDict.keys():
+            for key, value in signalDict[nid].items():
+                chanDesc[key] = value
+
+    def substituteUnits():
+        if 'units' in chanDesc.keys():
+            chanDesc["physicalQuantity"]['type'] = _base.Units(chanDesc["units"], 1)
+            del(chanDesc["units"])
+    nid = signal.Nid
+    chanDesc = _channelDict(signalroot, nid)
+    chanDesc = mergeSignalDict(nid)
+    chanDesc = substituteUnits()
     try:
-        units = _base.Units(sig, 1)
+        units = _base.Units(signal, 1)
         if units != 'unknown':
-            desc["physicalQuantity"]['type'] = units
+            chanDesc["physicalQuantity"]['type'] = units
     except:
         pass
-    return(desc)
+    return chanDesc
+
+
+def _channelDict(signalroot, nid):
+    """collects the parameters of a channel
+    called by _chanDesc"""
+    channelDict = {}
+    channelDict['name'] = signalroot.getNodeName().lower()
+    channelDict["active"] = int(signalroot.isOn())
+    channelDict["physicalQuantity"] = {'type': 'unknown'}
+    for sibling in signalroot.getDescendants():
+        if not sibling.Nid == nid:  # in case: DEVICE.STRUCTURE:SIGNAL
+            channelDict = _treeToDict(sibling, channelDict)
 
 
 def _treeToDict(node, Dict={}, name=None):
+    """generates a dict of the of a node structure
+    called by <multiple>"""
     try:
         if node.usage in ['ACTION', 'TASK', 'SIGNAL']:  # exclude by usage
             return Dict
@@ -227,37 +262,33 @@ def _treeToDict(node, Dict={}, name=None):
 
 
 def _write_signals(path, signals, t0):
-    R = []
+    """prepares signals and uploads to webarchive
+    called by <multiple>"""
+    logs = []
     t0 = _base.Time(t0).ns
     if len(signals) == 1:
-        sig = signals[0]
-        if sig.isSegmented():
-            R = []
-            for seg in _ver.xrange(sig.getNumSegments()):
-                data = sig.getSegment(seg).data()
-                dimof = _base.TimeArray(sig.getSegmentDim(seg)).ns
-                r = _if.write_data(path, data, dimof, t0)
-                R.append({"seg": seg, "rds": r})
-                print(seg, r)
-                if r.getcode() >= 400:
-                    print(r.content)
-                    return(R)
+        signal = signals[0]
+        if signal.isSegmented():
+            for segment in _ver.xrange(signal.getNumSegments()):
+                data = signal.getSegment(segment).data()
+                dimof = _base.TimeArray(signal.getSegmentDim(segment)).ns
+                log = _if.write_data(path, data, dimof, t0)
+                logs.append({"segment": segment, "log": log})
+                if logs[-1].getcode() >= 400:
+                    print(segment,logs[-1].content)
         else:
-            data = sig.data()
-            dimof = _base.TimeArray(sig.dim_of()).ns
-            R = _if.write_data(path, data, dimof, t0)
-            print(R)
-            if R.getcode() >= 400:
-                print(R.content)
-        return(R)
+            data = signal.data()
+            dimof = _base.TimeArray(signal.dim_of()).ns
+            logs.append(_if.write_data(path, data, dimof, t0))
+            if logs[-1].getcode() >= 400:
+                print(0,logs[-1].content)
     elif len(signals) > 1:
-        R = []
         data = []
         dimof = []
-        for sig in signals:
-            sig = sig.evaluate()
-            data.append(sig.data())
-            sigdimof = _base.TimeArray(sig.dim_of()).ns
+        for signal in signals:
+            signal = signal.evaluate()
+            data.append(signal.data())
+            sigdimof = _base.TimeArray(signal.dim_of()).ns
             if dimof==[]:
                 dimof = sigdimof
             else:
@@ -268,16 +299,18 @@ def _write_signals(path, signals, t0):
         data = _np.array(data)
         dimof = _np.array(dimof)
         N = 100000
-        for i in _ver.xrange(int((len(dimof)-1)/N+1)):
-            R.append(_if.write_data(path, data[:,i*N:(i+1)*N], dimof[i*N:(i+1)*N], t0))
-            if R[-1].getcode() >= 400:
-                print(R.content)
-        return R
+        for chunk in _ver.xrange(int((len(dimof)-1)/N+1)):
+            idx = chunk*N
+            logs.append(_if.write_data(path, data[:,idx:idx+N], dimof[idx:idx+N], t0))
+            if logs[-1].getcode() >= 400:
+                print(chunk,logs[-1].content)
+        return logs
 
 
-def _buildPath(node):  # , subsection=None):
-    from re import split
-    PathParts = split('::|:|\.|_', node.path.lstrip('\\'))
+def _buildPath(node):
+    """generates a path out of a treepath
+    called by uploadNode"""
+    PathParts = _re.split('::|:|\.|_', node.path.lstrip('\\'))
     KKS = PathParts[0]
     if PathParts[1] == 'EVAL':
         view = 'raw'  # will be cocking once supported
@@ -289,35 +322,31 @@ def _buildPath(node):  # , subsection=None):
     return(path)
 
 
-def uploadNode(node):  # , subsection=None):
-    '''
-    node = TreeNode or (tree, shot, path) or path -> (tree='w7X', shot=0)
-    e.g.:
-    uploadNode(("sandbox", -1, "\\KKS_EVAL::TOP.RESULTS:MYSECTION:MYIMAGE"))
-    '''
+def uploadNode(node, shot=0, treename='W7X'):
+    """upload a single node structure to the webarchive for kks_EVAL trees
+    uploadNode("\\KKS_EVAL::TOP.RESULTS:MYSECTION:MYIMAGE", -1, "sandbox")
+    """
     if isinstance(node, (_mds.treenode.TreeNode)):
         tree = node.tree
-    elif isinstance(node, (tuple, list)):
-        tree = _mds.Tree(node[0], node[1])
-        node = tree.getNode(node[2])
+        shot = int(tree.shot)
     else:
-        tree = _mds.Tree('W7X', 0)
+        tree = _mds.Tree(treename, shot)
         node = tree.getNode(node)
-    path = _buildPath(node)  # , subsection)
-    t0 = _base.Time(tree.getNode('\TIME.T0:IDEAL').data())
-    t1 = _base.Time(tree.getNode('\TIME.T1:IDEAL').data())
-    par, sig = _iterateDevices(node)
+    path = _buildPath(node)
+    T0 = _sup.getTiming(shot, 0)
+    T1 = _sup.getTiming(shot, 1)
+    parlog, sig = _deviceDict(node)
     if node.usage == "SIGNAL":
         if len(sig):  # prepend
             sig = [node] + sig
-            par["chanDescs"] = [_signalDict(node, node)] + par["chanDescs"]
+            parlog["chanDescs"] = [_chanDesc(node, node)] + parlog["chanDescs"]
         else:  # replace
             sig = [node]
-            par = {"chanDescs": [_signalDict(node, node)]}
-    par["shot"] = int(tree.shot)
+            parlog = {"chanDescs": [_chanDesc(node, node)]}
+#    parlog["shot"] = int(shot)
     r = [None, None]
-    r[0] = _if.write_logurl(path.url_parlog(), par, t0)
-    r[1] = _write_signals(path, sig, t1)
+    r[0] = _if.write_logurl(path.url_parlog(), parlog, T0)
+    r[1] = _write_signals(path, sig, T1)
     print(r)
     if not r[0].ok:
         print(r[0].content)
