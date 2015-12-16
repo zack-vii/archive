ed8eeaa4d1b59ffdd7ecd837b8f5e90cdf696f28
zack-vii <zack.vii@live.com>
2015-12-16 23:39:50 +0100

    pytest_threads

--- tdi/pytest.py
+++ tdi/pytest.py
@@ -1,14 +1,18 @@
 import MDSplus
+import threading as _th
+a = 0
 def pytest ( arg ):
+    global a
+    a = a+1
     try:    arg = arg.data()
     except: pass
     try:    arg = int(arg)
     except: arg = 0
     if arg>0:
-        x = MDSplus.TdiExecute('pytest($-1)',(int(arg),))
-        print(type(x),x)
-        return [arg]+list(x)
-    else:
-        return [arg]
+        print(a,arg)
+        threads = [_th.Thread(target=MDSplus.TdiExecute, args=('pytest($-1)',(arg,))) for i in range(1)]
+        for thread in threads: thread.start()
+        for thread in threads: thread.join()
+        print(a,arg)
 if __name__=='__main__':
-    print(pytest(5))
+    x = pytest(5)
