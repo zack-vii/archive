bcd4b24691e0285e3c5679fba50cae6b11ad1b1f
zack-vii <zack.vii@live.com>
2016-01-24 19:41:02 +0100

    expt not 'w7x'

--- tdi/getShotDB.py
+++ tdi/getShotDB.py
@@ -4,7 +4,7 @@ def getShotDB(expt,i=None):
     def getshots(expt_path):
         files = [f[len(expt):-5].split('_') for f in os.listdir(expt_path) if f.endswith('.tree') and f.startswith(expt+'_')]
         return [int(f[1]) for f in files if len(f)==2 and f[1]!='model']
-    expt_paths = os.getenv(expt+'_path').replace('~t','w7x').split(';')
+    expt_paths = os.getenv(expt+'_path').replace('~t',expt).split(';')
     if i is not None:
         shots = getshots(expt_paths[int(i)])
     else:
