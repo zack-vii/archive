f85026aa3cf7ab21b95af685a7ac11eef6eec53d
zack-vii <zack.vii@live.com>
2016-02-03 15:30:51 +0100

    output as string

--- tdi/archive_log.py
+++ tdi/archive_log.py
@@ -23,7 +23,7 @@ def archive_log(node, time=None, cache=None):
         else:
             time = base.TimeInterval(node.getNode('\TIME').data())
         """ request signal """
-        return interface.get_json(url, time=time)
+        return str(interface.get_json(url, time=time)['values'][-1])
     except:
         """ generate error message"""
         import getpass,sys
