595d37ac782494ecb059b082a9f328a487178045
zack-vii <zack.vii@live.com>
2016-02-11 10:40:26 +0100

    T2STR as single string or array of strings

--- tdi/T2STR.py
+++ tdi/T2STR.py
@@ -3,8 +3,13 @@ helper fuction that returns a human readable timestamp UTC if a W7X time
 T2STR( time )
 """
 from archive import base
-def T2STR(time):
+def T2STR(time, asarray=None):
     try: time = time.data()
     except: pass
-    try:    return base.Time(time).utc
-    except: return base.TimeArray(time).utc
+    try:
+        return base.Time(time).utc
+    except:
+        if asarray:
+            return base.TimeArray(time).utc
+        else:
+            return repr(base.TimeArray(time))
