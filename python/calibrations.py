26140fc4e6f6cb4a0a4cccd53ce1ebbd1df6b9f1
zack-vii <zack.vii@live.com>
2016-03-22 12:54:10 +0100

    new ECEcalib schema + test

--- python/calibrations.py
+++ python/calibrations.py
@@ -1,6 +1,5 @@
 def ECEcalib(sig, ecechannel):
     from archive import Time
-    from numpy import array
     from os import listdir, sys
     from json import load
     # paths to configuration files
@@ -9,30 +8,25 @@ def ECEcalib(sig, ecechannel):
     conf_dir = 'Config-files'
     cali_dir = 'Calibration-files'
     para_dir = 'Parameter-files'
+    # mapp_dir = 'Mapping-files'
+    # zoom_dir = 'Zoom-Parameter-files'
     path_root = path_win if sys.platform=='win32' else path_lx
 
-    def findSuitableFile(tstamp, dirname,versionfield):
-        dirpath = path_root+'/'+dirname
-        # selects the json file that is valid for the time of interest
-        jsonfiles =  [f for f in listdir(dirpath) if f.endswith('.json')]
-        jsonfiles.reverse()  # newest first
-        valid = [(0,0,'')]*len(jsonfiles)
-        # read valid_since fields
-        for i,filename in enumerate(jsonfiles):
-            with open(dirpath+'/'+filename) as f:
-                out = load(f)
-            try:    val = Time(out['Valid_Since']).ns
-            except: val = 0
-            ver = int(out.get(versionfield,0))
-            valid[i] = (val,ver,filename)
-        valid = array(valid, dtype=[('d',long),('v',int),('f','S64')])
-        valid[::-1].sort(order=['d','v'])
-        # get the latest version that is anterior to the time of interest
-        for v in valid:
-            print(v,tstamp)
-            if v[0]<tstamp:
-                return dirname+'/'+v[2]
-        return None
+    def pickECEfile( utcin, search_dir ):
+        def parseECEfilname(filname):
+            from datetime import datetime
+            datestr = filname.split('_')[1]
+            dtobject = datetime.strptime( datestr,'%y%m%d-%H%M')
+            return Time(datetime.strftime( dtobject, '%Y-%m-%d %H:%M:%S:%f'))
+        dirs = listdir(path_root+'/'+search_dir)
+        filenames = []
+        for filen in dirs:
+            if filen.startswith('QME-'):
+                tfil = parseECEfilname(filen)
+                if tfil<utcin:
+                    filenames.append(search_dir+'/'+filen)
+        filenames.sort()
+        return filenames[-1]
 
     def getOffset(sig):
         dim = sig.dim_of()
@@ -48,13 +42,13 @@ def ECEcalib(sig, ecechannel):
             if t>tlim: break  # this way i is number of accumulated samples
             value+= data[i]
         return value/i
-    timestamp = Time(sig.args[2][0][2]).ns
+    timestamp = Time(sig.args[2][0][0])
     # find the files that apply to the time of interest
-    conf_file = findSuitableFile(timestamp, conf_dir, 'Version')
+    conf_file = pickECEfile(timestamp, conf_dir)
     if conf_file is None: return
-    cali_file = findSuitableFile(timestamp, cali_dir, 'Calib_File_Version')
+    cali_file = pickECEfile(timestamp, cali_dir)
     if cali_file is None: return
-    para_file = findSuitableFile(timestamp, para_dir, 'Version')
+    para_file = pickECEfile(timestamp, para_dir)
     if para_file is None: return None,None,None,{}
     # get frequencies and bittovolt
     with open(path_root+'/'+conf_file) as f: cfg = load(f)
--- python/testarchive.py
+++ python/testarchive.py
@@ -11,3 +11,6 @@ print(repr(archive.TimeInterval()))
 print(1,archive.Time('2009-02-13T23:31:30.123456789Z')==1234567890123456789)
 print(2,'Thu Aug 12 13:14:15 2010'==archive.Time('2010-08-12 13:14:15', local=True).local)
 print(3,archive.Path()._set_streamgroup('streamgroup')._set_stream('stream').path_data(channel=7,scaled='scaled',time=archive.TimeInterval())=='/ArchiveDB/codac/W7X/streamgroup/stream_DATASTREAM/7/scaled')
+sig=archive.interface.read_signal('/ArchiveDB/codac/W7X/CoDaStationDesc.16007/DataReductionProcessDesc.17547_DATASTREAM',time=[1458645620907337340,1458645621902337341],channel=17);
+print(4,sig.data()[0]==4645)
+print(5,archive.calibrations.ECEcalib(sig,17)[1]==0.0007008221583731498)
