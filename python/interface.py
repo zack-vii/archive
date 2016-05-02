949cd2576098609fcd959fcdfca8d821e0199930
zack-vii <timo.schroeder@ipp.mpg.de>
2016-05-02 16:49:57 +0200

    ifxed accidentally removed separator

--- python/interface.py
+++ python/interface.py
@@ -123,7 +123,7 @@ def uploadH5(path, h5file, delete=False):
     stream = path.stream
     try:
         headers = {'Content-Type': 'application/x-hdf'}
-        link = path.url_streamgroup()+'/?dataPath=data'+stream+'/&timePath=data/timestamps'
+        link = path.url_streamgroup()+'/?dataPath=data/'+stream+'/&timePath=data/timestamps'
         f = open(h5file, 'rb')
         try:
             result = post(link, headers=headers, data=f)
